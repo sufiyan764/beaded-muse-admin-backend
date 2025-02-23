f6f8d73d4b9a0bd92451cb6bd3c2772deef00ac2
sufiyan764 <sufiyan764@gmail.com>
2025-02-22 22:09:26 -0500

    fixed counter issue

--- src/api/models/Common.js
+++ src/api/models/Common.js
@@ -2,14 +2,14 @@
 
 import { MONGO_MODEL } from '.'
 
-const counter = async ({counterName}) => {
+const counter = async (counterName) => {
   const sequenceCount = await MONGO_MODEL.mongoFindOneAndUpdate('counters', {id: 1}, {
     $inc: {
       [counterName]: 1
     }
   });
   
-  return sequenceCount?.value?.counters[counter] || 1
+  return sequenceCount?.value?.[counter] || 1
 }
 
 const getAdminName = async (_id) => {
